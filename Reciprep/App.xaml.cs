6e61d7fb26d660255096af78215e3d35a54dd604
Bradley Pirie <81576268+bppirie@users.noreply.github.com>
2023-04-08 20:41:20 -0400

    Initial commit

---
+++ Reciprep/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Reciprep;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Reciprep/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Reciprep;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
